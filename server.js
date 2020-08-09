36f90675916a6bd93ae895cdca371d83df164afb
radwaaly <radwaaly@users.noreply.github.com>
2020-08-09 19:07:01 +0200

    modify style.css to adjust the gui get data from weather api send the retrieved data, the user feeling, and date to server.js get the saved data from the server.js update view with the retrieved data

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +17,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port   = 8000;
+const server = app.listen(port,listening);
+
+function listening(){
+    console.log(`server running on port ${port}`);
+}
+
+// ROUTES!
+app.get('/get',getData);
+function getData(req,res){
+    console.log('get request from server');
+    res.send(projectData);
+}
+
+app.post('/add', postData);
+function postData (req,res){
+    let data = req.body; 
+
+    projectData.temp        = data.main.temp;
+    projectData.feelings    = data.feelings;
+    projectData.date        = data.date;
+    //projectData["temp"]= data.temp;
+    //console.log(projectData["temp"]);
+    console.log(projectData.temp);
+    console.log(projectData.feelings);
+    console.log(projectData.date);
+    res.end();
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,66 @@
-/* Global Variables */
+//const { json } = require("body-parser");
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+/* Global Variables */
+const apiKey = '1a58db9f1916493e52f2a2f455947fc5';
+//http://api.openweathermap.org/data/2.5/weather?units=metric&zip=94040&appid=1a58db9f1916493e52f2a2f455947fc5
+let baseURL  = 'http://api.openweathermap.org/data/2.5/weather?units=metric';
+
+
+const performAction = async () => {
+    //get user data
+    const zipcode  = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    //set weather api url
+    //http://api.openweathermap.org/data/2.5/weather?units=metric&zip=94040&appid=1a58db9f1916493e52f2a2f455947fc5
+    const completeURL = `${baseURL}&zip=${zipcode}&appid=${apiKey}`;
+    //console.log(completeURL);
+    //get data from weather api
+    let data = await getApiData(completeURL);
+    data.feelings = feelings;
+
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let month = d.getMonth() +1;//0 is january
+    let newDate = month +'.'+ d.getDate()+'.'+ d.getFullYear();
+    data.date = newDate;
+
+    //post data to server.js
+    await postData('/add' , data)
+
+    //get the posted data from server.js
+    let projectData = await getApiData('/get');
+    //console.log(projectData);
+    //update view
+    document.getElementById('date')   .innerHTML  = projectData.date;
+    document.getElementById('temp')   .innerHTML  = projectData.temp;
+    document.getElementById('content').innerHTML  = projectData.feelings;
+};
+
+const getApiData = async (url = '') => {
+    const res = await fetch(url);
+    try {
+        const data = await res.json();
+        //console.log(data.main.temp);
+        return data;
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+};
+
+// Async POST
+const postData = async ( url = '', data = {}) => {
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header        
+  });
+};
+
+//add event listener click on generate button
+document.getElementById('generate').addEventListener('click' , performAction);
+
